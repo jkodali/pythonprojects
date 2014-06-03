b2d494e78c14c01eddb0df8a6ed271d899ee25d5
Jeevan Kodali <jeevan.kodali@morningstar.com>
2014-06-03 17:01:13 -0500

    Dice: made the code more modular and extracted main table from full html

--- jobsearch/Dice.py
+++ jobsearch/Dice.py
@@ -1,7 +1,24 @@
 import urllib2
 import re
-response = urllib2.urlopen("http://www.dice.com/job/results/20001?b=7&caller=searchagain&n=50&q=technology+manager&src=19&x=all&p=z")
-html = response.read()
-response.close()
-p = re.compile('summary')
-print p.match(html)
+
+
+
+def processDiceData():
+	response = urllib2.urlopen("http://www.dice.com/job/results/20001?b=7&caller=searchagain&n=50&q=technology+manager&src=19&x=all&p=z")
+	fullhtmlString = response.read()
+	response.close()
+	tablestart = re.compile('<table class="summary" cellspacing="0">')
+	tablestartmatch = tablestart.search(fullhtmlString)
+
+	fromtableString = fullhtmlString[tablestartmatch.start():]
+	tableend = re.compile("</table>")
+	tableendmatch = tableend.search(fromtableString)
+	maintableHTML = fromtableString[:tableendmatch.end()]
+
+	print maintableHTML
+
+def main():
+	processDiceData()
+
+if __name__ == "__main__":
+	main()
