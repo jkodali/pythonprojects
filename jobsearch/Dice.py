db7c90db93b0d9a80b37abc68aec30725ab8a516
jkodali <kjeevan_kumar@yahoo.com>
2014-06-04 08:16:08 -0500

    Dice - starting to use xml parser

--- jobsearch/Dice.py
+++ jobsearch/Dice.py
@@ -1,9 +1,9 @@
 import urllib2
 import re
+import xml.etree.ElementTree as ET
 
 
-
-def processDiceData():
+def processDiceDataFromScraping():
 	response = urllib2.urlopen("http://www.dice.com/job/results/20001?b=7&caller=searchagain&n=50&q=technology+manager&src=19&x=all&p=z")
 	fullhtmlString = response.read()
 	response.close()
@@ -15,10 +15,13 @@ def processDiceData():
 	tableendmatch = tableend.search(fromtableString)
 	maintableHTML = fromtableString[:tableendmatch.end()]
 
+	#tree = ET.fromstring(maintableHTML)
+
 	print maintableHTML
+	#print tree.tag
 
 def main():
-	processDiceData()
+	processDiceDataFromScraping()
 
 if __name__ == "__main__":
 	main()
