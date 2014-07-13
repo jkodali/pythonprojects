d1890324baf9f8d9851b7cc0f9347bf2d6b62332
jkodali <kjeevan_kumar@yahoo.com>
2014-07-13 00:41:24 -0500

    better pagination handling - taking advantage of django's paginaton functionality

--- jobsearch/website/joblist/getjoblist/urls.py
+++ jobsearch/website/joblist/getjoblist/urls.py
@@ -4,5 +4,5 @@ from getjoblist import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<jobsite>\w+)/(?P<city>\w+)/(?P<searchstring>[\w\+]+)/(?P<pageNumber>[0-9]+)?$', views.joblist, name='joblist')
+    url(r'^(?P<jobsite>\w+)/(?P<city>\w+)/(?P<searchstring>[\w\+]+)$', views.joblist, name='joblist')
 ]
--- jobsearch/website/joblist/getjoblist/views.py
+++ jobsearch/website/joblist/getjoblist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from models import JobList
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def index(request):
@@ -11,16 +12,19 @@ def index(request):
 	for jobsite in jobsitelist:
 		for city in citylist:
 			for searchstring in searchstringlist:
-				message = message + "<a href='./%s/%s/%s'>%s - %s - %s</a><br />" % (jobsite, city, searchstring, jobsite, city, searchstring)
+				message = message + "<a href='./%s/%s/%s?page=1'>%s - %s - %s</a><br />" % (jobsite, city, searchstring, jobsite, city, searchstring)
 	return HttpResponse(message) 
 
-def joblist(request, jobsite, city, searchstring, pageNumber):
-	if pageNumber is None:
-		pageNumber = 1
-	else:
-		pageNumber = int(pageNumber)
+def joblist(request, jobsite, city, searchstring):
 	fulljoblist = JobList.objects.filter(JobSite=jobsite, SearchString=searchstring, CityToSearch=city)
-	joblist = fulljoblist[((pageNumber-1)*50+1):((pageNumber)*50)]
-	#return HttpResponse(joblist[0].City)
-	context = {'joblist': joblist, 'nextPageNumber': pageNumber+1, 'prevPageNumber': pageNumber-1, 'total': len(fulljoblist), 'start': (((pageNumber-1)*50+1)), 'end': (((pageNumber)*50)) }
+	paginator = Paginator(fulljoblist, 50)
+
+	pageNumber = request.GET.get('page')
+	try:
+		joblist = paginator.page(pageNumber)
+	except PageNotAnInteger:
+		joblist = paginator.page(1)
+	except EmptyPage:
+		joblist = paginator.page(1)
+	context = {'joblist': joblist }
 	return render(request, 'getjoblist/index.html', context)
